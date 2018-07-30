910ac00c7a236bdd2b9068cfc49905a55d35a50b
Lisandro Corbalan <lmcorbalan@gmail.com>
2018-07-29 23:05:57 -0300

    Add some style

--- src/App.js
+++ src/App.js
@@ -1,131 +1,156 @@
-import React, { Component } from 'react'
-import DarkRoomContract from '../build/contracts/DarkRoom.json'
-import getWeb3 from './utils/getWeb3'
+import React, { Component } from 'react';
+import DarkRoomContract from '../build/contracts/DarkRoom.json';
+import getWeb3 from './utils/getWeb3';
 
-import './css/oswald.css'
-import './css/open-sans.css'
-import './css/pure-min.css'
-import './App.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faThumbsUp, faThumbsDown)
+
+import './css/oswald.css';
+import './css/open-sans.css';
+import './css/pure-min.css';
+import './App.css';
+
+const IconVoteYes = () => <FontAwesomeIcon className="icon__yes" icon='thumbs-up' />;
+const IconVoteNo = () => <FontAwesomeIcon className="icon__no" icon='thumbs-down' />
 
 class App extends Component {
   state = {
     YES: 0,
     NO: 0,
-    shouldDisableVotes: true,
+    shouldDisableVotes: true
   };
 
   darkRoomInstance = null;
 
   account = '';
 
-  vote = (option) => () => {
-    console.log('voting', option);
-    this.setState({ shouldDisableVotes: true });
-    this.darkRoomInstance.vote(option, {from: this.account})
-      .then((result) => {
-        console.log('vote', result);
-      });
-  }
+  vote = option => () => {
+    this.darkRoomInstance
+      .vote(option, { from: this.account })
+      .then(result => {
+        const votedOption = option ? 'YES' : 'NO';
+        const tempOption = this.state[votedOption] + 1;
 
-  onNewVote = (error, result) => {
-    console.log(error);
+        this.setState({
+          shouldDisableVotes: true,
+          [votedOption]: tempOption
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  handleNewVoteEvent = (error, result) => {
     if (!error) {
-      this.setState({ YES: result.args.totalYes.toNumber() });
-      this.setState({ NO: result.args.totalNo.toNumber() });
+      this.setState({
+        YES: result.args.totalYes.toNumber(),
+        NO: result.args.totalNo.toNumber()
+      });
+    } else {
+      console.error(error);
     }
-  }
+  };
 
   componentWillMount() {
     // Get network provider and web3 instance.
     // See utils/getWeb3 for more info.
-
     getWeb3
-    .then(results => {
-      this.setState({
-        web3: results.web3
-      })
+      .then(results => {
+        this.setState({
+          web3: results.web3
+        });
 
-      // Instantiate contract once web3 provided.
-      this.instantiateContract()
-    })
-    .catch(() => {
-      console.log('Error finding web3.')
-    })
+        // Instantiate contract once web3 provided.
+        this.instantiateContract();
+      })
+      .catch(() => {
+        console.log('Error finding web3.');
+      });
   }
 
   instantiateContract() {
-    const contract = require('truffle-contract')
-    const darkRoom = contract(DarkRoomContract)
-    darkRoom.setProvider(this.state.web3.currentProvider)
+    const contract = require('truffle-contract');
+    const darkRoom = contract(DarkRoomContract);
+    darkRoom.setProvider(this.state.web3.currentProvider);
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      darkRoom.deployed().then((instance) => {
-        this.darkRoomInstance = instance
-        this.account = accounts[0];
-        console.log(accounts);
-        console.log(this.account);
+      darkRoom
+        .deployed()
+        .then(instance => {
+          this.darkRoomInstance = instance;
+          this.account = accounts[0];
+          this.darkRoomInstance.NewVote(this.handleNewVoteEvent);
 
-        this.darkRoomInstance.NewVote(this.onNewVote);
+          return this.darkRoomInstance.getVoter({ from: this.account });
+        })
+        .then(result => {
+          console.log('voter', result);
+          const [_, myVote, hasVoted] = result;
+          this.setState({ shouldDisableVotes: hasVoted });
 
-        return this.darkRoomInstance.getVoter({from: this.account});
+          if (hasVoted) {
+            this.setState({ myVote });
+          }
 
-      }).then(result => {
-        console.log(result)
-
-        const [_,myVote, hasVoted] = result;
-
-        console.log('myVote', myVote);
-        console.log('hasVoted', hasVoted);
-
-        this.setState({ shouldDisableVotes: hasVoted });
-
-        if (hasVoted) {
-          this.setState({ myVote })
-        }
-
-        return this.darkRoomInstance.getResults({from: this.account});
-      }).then(result => {
-        console.log('results', result)
-        this.setState({ YES: result[0].toNumber() });
-        this.setState({ NO: result[1].toNumber() });
-        const YES = result[0].toNumber();
-        const NO = result[1].toNumber();
-        console.log('YES', YES)
-        console.log('NO', NO)
-      })
-    })
+          return this.darkRoomInstance.getResults({ from: this.account });
+        })
+        .then(result => {
+          console.log('results', result);
+          this.setState({ YES: result[0].toNumber() });
+          this.setState({ NO: result[1].toNumber() });
+        });
+    });
   }
 
   render() {
-
-    const vote = this.state.shouldDisableVotes
-      ? <h2>YOU VOTED: {this.state.myVote ? 'YES' : 'NO'}</h2>
-      : (
+    const vote = this.state.shouldDisableVotes ? (
+      <div className="vote-actions">
         <div>
-          <button onClick={this.vote(true)}>YES</button>
-          <button onClick={this.vote(false)}>NO</button>
+          <h2>You Voted</h2>
         </div>
-      )
+        <div>
+          <span className={`vote-btn  ${this.state.myVote ? 'current-vote__yes' : 'current-vote__no'}`} >
+          {this.state.myVote ? <IconVoteYes /> : <IconVoteNo />}
+          </span>
+        </div>
+      </div>
+    ) : (
+      <div className="vote-actions">
+        <div>
+          <h2>What are you going to vote?</h2>
+        </div>
+        <div>
+          <button className="vote-btn vote-btn__yes" onClick={this.vote(true)}><span><IconVoteYes /></span></button>
+          <button className="vote-btn vote-btn__no" onClick={this.vote(true)}><span><IconVoteNo /></span></button>
+        </div>
+      </div>
+    );
 
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+          <a href="#" className="pure-menu-heading pure-menu-link">
+            The Dark Room
+          </a>
         </nav>
         <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>VOTE: YES - NO</h1>
-              <p>YES: {this.state.YES}</p>
-              <p>YES: {this.state.NO}</p>
-              {vote}
-            </div>
+          <div>
+            <h2>What people have voted so far</h2>
           </div>
+          <div className="current-votes">
+            <h2><IconVoteYes /> {this.state.YES}</h2>
+            <h2><IconVoteNo /> {this.state.NO}</h2>
+          </div>
+          {vote}
         </main>
       </div>
     );
   }
 }
 
-export default App
+export default App;
